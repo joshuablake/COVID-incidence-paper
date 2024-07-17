8f934950e787519b80004c547ebe5b840fc266cd
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2024-07-17 11:11:42 +0100

    First go at components figure

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-main.pdf: main.tex figures/incidence.pdf references.bib .PHONY
+main.pdf: main.tex figures/incidence.pdf references.bib figures/components.pdf .PHONY
 	./latexrun $<
 
 figures/incidence.pdf: figures/incidence.R figures/utils.R model-outputs/mechanistic/predictive.csv model-outputs/phenomenological/region.rds model-outputs/poststrat.csv
+	Rscript $<
+
+figures/components.pdf: figures/components.R figures/utils.R model-outputs/mechanistic/params.csv
 	Rscript $<
 
 .PHONY: FORCE
---
+++ figures/components.R
@@ -0,0 +1,107 @@
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+library(purrr)
+library(lubridate)
+library(tidybayes)
+library(tidyr)
+source(here::here("figures/utils.R"))
+
+start_date = ymd("2020-08-31")
+
+tbl_draws = readr::read_csv(here::here("model-outputs", "mechanistic", "params.csv"))
+
+tbl_rw = tbl_draws |>
+    # Find beta parameters and parse name index
+    filter(stringr::str_starts(parameter, "beta"), iteration >= 500e3) |>
+    extract(
+        parameter,
+        into = c("parameter", "i"), 
+        regex = "^([^\\[]+)(\\[[0-9]+\\])?$"
+    ) |>
+    transmute(
+        region = normalise_region_names(region),
+        # Convert 0-indexed (python) numbers to 1-indexed (R) numbers
+        .chain = factor(chain + 1),
+        .iteration = iteration + 1,
+        .draw = max(.iteration) * (as.integer(.chain) - 1) + .iteration,
+        # Parse index of beta
+        i = if_else(
+            i == "",
+            NA_integer_,
+            # Remove first and last character from i
+            as.integer(substr(i, 2, nchar(i) - 1))
+        ),
+        value,
+    ) |>
+    # Converted from non-centered log-scale in the model to
+    # centered natural scale for processing
+    mutate(
+        .by = c(region, .draw),
+        value = if_else(
+            i == 0,
+            0,
+            value * exp(value[i == 0])
+        ),
+    ) |>
+    # RW1 hence cumulative sum gives actual value
+    group_by(region, .draw) |>
+    arrange(i, .by_group = TRUE) |>
+    mutate(
+        beta = value |>
+            cumsum() |>
+            exp()
+    )
+
+# A violin plot of the posterior distribution of beta for each region and week.
+p_walk = tbl_rw |>
+    filter(region == "London") |>
+    mutate(date = start_date + i * 7) |>
+    ggplot(aes(factor(date), beta)) +
+    stat_slab(side = "both") +
+    facet_wrap(~region, ncol = 2) +
+    standard_plot_theming() +
+    scale_y_log10(
+        breaks = c(0.5, 1, 2, 0.67, 1.5),
+        limits = c(0.5, NA),
+        minor_breaks = numeric()
+    ) +
+    # tick labels every 3 weeks
+    scale_x_discrete(
+        breaks = function(x) x[c(TRUE, rep(FALSE, 3))]
+    ) +
+    geom_hline(yintercept = 1, linetype = "dashed", alpha = 0.5) +
+    # Angle text for readability
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(
+        x = "Date",
+        y = expression(beta[t])
+    )
+
+# susceptibility of children
+p_susceptibility = tbl_draws |>
+    filter(parameter == "matrix_modifiers") |>
+    mutate(value = exp(value), region = normalise_region_names(region)) |>
+    # dot interval plot with median and 95% CrI by region
+    group_by(region) |>
+    median_qi(value) |>
+    ggplot(aes(region, value)) +
+    geom_pointrange(aes(ymin = .lower, ymax = .upper), size = 0.1) +
+    labs(
+        x = "Region",
+        y = "Relative susceptibility of children"
+    ) +
+    standard_plot_theming() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    geom_hline(yintercept = 1, linetype = "dashed", alpha = 0.5)
+
+# Combine plots with alphabetic labels
+ggsave(
+    filename = here::here("figures/components.pdf"),
+    plot = p_walk + p_susceptibility +
+        plot_annotation(tag_levels = 'A'),
+    width = 11.4,
+    height = 5.5,
+    units = "cm",
+    dpi = 300
+)
