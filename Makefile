00fc20bc4045d3031ccf9b85e1e85ca6b6a5a713
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2026-06-10 07:57:39 +0100

    Add spaghetti plot

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-main.pdf: main.tex .PHONY references.bib figures/incidence_England.pdf figures/incidence_stratified.pdf figures/incidence_cutoffs.pdf figures/incidence_cutoffs_region.pdf figures/rw_London.pdf figures/rw_non-London.pdf figures/children.pdf figures/gof.pdf
+main.pdf: main.tex .PHONY references.bib figures/incidence_England.pdf figures/incidence_stratified.pdf figures/incidence_cutoffs.pdf figures/incidence_cutoffs_region.pdf figures/rw_London.pdf figures/rw_non-London.pdf figures/children.pdf figures/gof.pdf figures/spaghetti.pdf
 	python3 latexrun $<
 
-figures/incidence_England.pdf figures/incidence_stratified.pdf: figures/incidence.R figures/utils.R model-outputs/mechanistic/predictive.csv.gz model-outputs/phenomenological/region.rds model-outputs/phenomenological/age.rds model-outputs/poststrat.csv
+figures/incidence_England.pdf figures/incidence_stratified.pdf figures/spaghetti.pdf: figures/incidence.R figures/utils.R model-outputs/mechanistic/predictive.csv.gz model-outputs/phenomenological/region.rds model-outputs/phenomenological/age.rds model-outputs/poststrat.csv
 	Rscript $<
 
 figures/incidence_cutoffs.pdf figures/incidence_cutoffs_region.pdf: figures/incidence_cutoffs.R figures/utils.R model-outputs/mechanistic/incidence_summary_statistics.csv
--- figures/incidence.R
+++ figures/incidence.R
@@ -15,14 +15,15 @@ tbl_phenomenological_age = load_backcalc_age() |>
 full_predict = load_seir_predictive()
 poststrat_table = load_poststrat_table()
 
-p_incidence_England = bind_rows(
+df_incidence = bind_rows(
     tbl_phenomenological_England |>
         mutate(model = "Phenomenological"),
     full_predict |>
         poststratify_SEIR(poststrat_table, new_pcr_pos) |>
         mutate(model = "Mechanistic") |>
         rename(incidence = val),
-) |>
+)
+p_incidence_England = df_incidence |>
     ggplot(aes(date, incidence, colour = model, fill = model)) +
     stat_lineribbon(alpha = 0.4, linewidth = 0.5, .width = 0.95) +
     incidence_plot_theming() +
@@ -32,6 +33,39 @@ save_plot(
     filename = "incidence_England.pdf",
     plot = p_incidence_England,
     height = 7
+)
+
+
+spaghetti_plot = function(
+    model, n_draws = 50,
+    start_date = lubridate::as_date("2020-12-01"),
+    end_date = lubridate::as_date("2021-01-15")
+) {
+    draw_nums = df_incidence |>
+        filter(model == !!model) |>
+        distinct(.draw) |>
+        pull(.draw)
+    df_incidence |>
+        filter(
+            model == !!model, .draw %in% sample(draw_nums, n_draws),
+            between(date, start_date, end_date)
+        ) |>
+        ggplot(aes(date, incidence, group = .draw, colour = as.factor(.draw))) +
+        geom_line(alpha = 0.3, linewidth = 0.5) +
+        incidence_plot_theming() +
+        theme(legend.position = "none")
+        # scale_y_continuous(
+        #     breaks = scales::breaks_extended(10),
+        #     labels = scales::label_percent(accuracy = 0.01)
+        # )
+}
+p_spaghetti = spaghetti_plot("Mechanistic") / spaghetti_plot("Phenomenological") +
+    plot_layout(guides = "collect") +
+    plot_annotation(tag_levels = 'A')
+save_plot(
+    filename = "spaghetti.pdf",
+    plot = p_spaghetti,
+    full_page = TRUE
 )
 
 p_incidence_age = bind_rows(
